ac2a5bafa3a516e8e5a121db00d953977a558519
thuhuong17 <huongfd@gmai.com>
2024-02-24 16:12:20 +0700

    upload baiSo10

---
+++ bai10/bai10.js
@@ -0,0 +1,6 @@
+console.log("Hello world from HTML".toUpperCase());
+let a = 5, b=6, c = 6.9, d = '5', e=+d;
+// hoac e hoac f deu giong nhau
+let f = Number(d)
+console.log('e value: ', e, '; type: ', typeof e);
+console.log(a === e, 'a: ', typeof a, '; d: ', typeof d);
