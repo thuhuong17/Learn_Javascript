eb1d669acee7805f156b932a809fc9796dd4c287
thuhuong17 <huongfd@gmai.com>
2024-03-04 21:48:06 +0700

    upload baiSo14

---
+++ baiSo14/bai14.js
@@ -0,0 +1,15 @@
+console.log("Hello from Hello WWorld")
+
+// key: value
+let a = 5, b= '5';
+console.log(`compare a va b: a = ${a}, b = ${b}, results`, a===b);
+console.log(`compare a va b: a = ${a}, b = ${b}, results`, a==b);
+let c = null;
+let d = '';//kb đối string empty
+let e = {};//kb đối tượng empty
+let f = [];// kb arr empty
+console.log(`check value: ${c}`);
+console.log(`check value: ${d}`);
+console.log(`check value: ${e}`);
+console.log(`check value: ${f}`);
+
