7d2a91e1acf02bd2e04111221339da3e6d4a19a7
thuhuong17 <huongfd@gmai.com>
2024-03-04 20:34:25 +0700

    upload baiSo13

---
+++ baiSo13/bai13.js
@@ -0,0 +1,11 @@
+console.log("Hello from Hello WWorld")
+
+// key: value
+let arr = ['Huong', 'Hoa', 'Hoe'];
+console.log('type of arr', typeof arr, arr);
+console.log('in ra phan tu dau tien', arr[0]);
+let family = [
+    {name:'Huong', age:'18'},
+    {name:'Kim', age:'19'}
+]
+console.log('print family', family)
