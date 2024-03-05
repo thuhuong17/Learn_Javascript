4782ee63dcecebcdde11839d7563946803f33022
thuhuong17 <huongfd@gmai.com>
2024-03-05 21:49:08 +0700

    upload baiSo15

---
+++ baiSo15/bai15.js
@@ -0,0 +1,14 @@
+console.log("Hello from Hello WWorld")
+
+// fors lops
+for(let i=0; i<=10;i++){
+    console.log(`check variable: `, i);
+}
+// dung let
+let arr=['Huong', 'Hoa', 'Hoe'];
+for(let i =1; i<arr.length; i++)
+{
+    console.log(`check result: `, arr[i]);
+}
+// dung var
+let arr1=
